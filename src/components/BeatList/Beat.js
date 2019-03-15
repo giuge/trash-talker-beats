204b14f911b4f5b50ad8bdd1a8a1d58d1bbb2237
Giuseppe Germinaro <g.germinaro88@gmail.com>
2019-03-15 19:26:50 +0100

    Added ability to download preview file

--- src/components/BeatList/Beat.js
+++ src/components/BeatList/Beat.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 import { IconContext } from 'react-icons'
-import { MdShoppingCart, MdPlayArrow } from 'react-icons/md'
+import { MdShoppingCart, MdPlayArrow, MdFileDownload } from 'react-icons/md'
 
 import { withAllContext } from '../../context/AllContext'
 
@@ -155,11 +155,22 @@ const InCart = styled.button`
   }
 `
 
+const ActionsContainer = styled.div`
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+`
+
+const DownloadLink = styled.a`
+  display: block;
+  margin-right: 16px;
+`
+
 const Product = props => {
   const { beat, context } = props
   const image = beat ? beat.images[0].localFile.childImageSharp.fixed.src : null
 
-  const handleClick = () => {
+  const handleClick = e => {
     context.interface.selectPreview(beat)
   }
 
@@ -189,7 +200,7 @@ const Product = props => {
 
   return (
     <ListItem
-      onClick={() => handleClick(beat.preview)}
+      onClick={e => handleClick(e)}
       highlighted={previewFile.id === beat.id}
     >
       <Details>
@@ -209,11 +220,30 @@ const Product = props => {
         <h3>{beat.title}</h3>
         <Tags>
           {beat.tags.map(t => (
-            <li key={t} title="Click to filter">{`#${t}`}</li>
+            <li key={t}>{`#${t}`}</li>
           ))}
         </Tags>
       </Details>
-      {renderButton()}
+      <ActionsContainer>
+        <DownloadLink
+          title="Download demo"
+          onClick={e => e.stopPropagation()}
+          href={beat.preview.publicURL}
+          download={`trashtalkerbeats_${beat.preview.name}.${
+            beat.preview.extension
+          }`}
+        >
+          <IconContext.Provider
+            value={{
+              size: 24,
+              color: '#376280',
+            }}
+          >
+            <MdFileDownload />
+          </IconContext.Provider>
+        </DownloadLink>
+        {renderButton()}
+      </ActionsContainer>
     </ListItem>
   )
 }
